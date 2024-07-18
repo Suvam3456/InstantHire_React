8fb8dc53b7dd21a2db8b760a3514d2cd8b04829b
SUVAM AGRAWAL <99579881+Suvam3456@users.noreply.github.com>
2024-07-18 12:10:56 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <MainHeader>
       <NavLink to="/">
-        <img src="./images.jpeg" alt="logo" className="logo" />
+        <img src="./images/logo/png" alt="logo" className="logo" />
       </NavLink>
       <Navbar />
     </MainHeader>
