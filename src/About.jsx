3fb7b8fdec4aafa110a94ba091a7ba64826ae55e
SUVAM AGRAWAL <99579881+Suvam3456@users.noreply.github.com>
2024-07-18 12:40:55 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -4,12 +4,10 @@ import { useGlobalContext } from "./context";
 
 const About = () => {
   const { udpateAboutPage } = useGlobalContext();
-  Welcome to [Website Name] – Your Gateway to Cutting-Edge Technology Solutions!
 
-At Techfusion, we are committed to empowering businesses and individuals with innovative and reliable tech solutions. Our mission is to drive digital transformation and ensure seamless integration of technology into your daily operations. We specialize in providing a comprehensive range of services designed to meet the evolving needs of the modern world.
   useEffect(() => udpateAboutPage(), []);
 
-  // return <HeroSection/>;
+  return <HeroSection/>;
 };
 
 export default About;
